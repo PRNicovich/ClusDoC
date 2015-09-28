865a365ec606612a447960e0f98b3517e279c545
Thibault <t.tabarin@unsw.edu.au>
2015-09-28 17:38:43 +1000

    update for getAveDc

--- GetAveDC.m
+++ GetAveDC.m
@@ -1,8 +1,8 @@
 function [ AveDC_Ch1, AveDC_Ch2 ] = GetAveDC
-%% Extract and calculate the average DC for 
+%% Extract and calculate the average DC for all the molecules for
 % each channel Ch1 and Ch2
 % Just run the function  and it will ask you the file to use, take the file Data_for_cluster_Analysis: 
-% e.g. located Output1\DofC_Result\Data_for_cluster_Analysis
+%  located at e.g. Output1\DofC_Result\Data_for_cluster_Analysis
 % Preferentially use the output from the GUI
         [File_name,Path_name] = uigetfile({'*.mat'},'Choose Data_for_Cluster_Analysis');
         cd(Path_name)
